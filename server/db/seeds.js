7013327fa5a476d5b8184116f49edc19d7cb84a9
Ahmed Hamed <tunashark@gmail.com>
2020-07-03 15:19:31 +0100

    basic seeds structure

---
+++ server/db/seeds.js
@@ -0,0 +1,191 @@
+use propertys;
+db.dropDatabase()
+
+db.propertys.insertMany([
+    {
+        "name": "Ahmed Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Nourhan Station",
+        "value": 60,
+        "rent_value": 6
+    },
+    {
+        "name": "Ronan Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Fady Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Ollies Street",
+        "value": 50,
+        "rent_value": 10
+    },
+    {
+        "name": "Keiths Street",
+        "value": 50,
+        "rent_value": 10
+    },    
+    {
+        "name": "Saras Street",
+        "value": 50,
+        "rent_value": 10
+    },    
+    {
+        "name": "Eds Street",
+        "value": 50,
+        "rent_value": 10
+    },
+    {
+        "name": "Adams Street",
+        "value": 50,
+        "rent_value": 10
+    },
+    {
+        "name": "Alex Chance Cards",
+        "value": 0,
+        "rent_value": 0
+    },
+    {
+        "name": "Jail",
+        "value": 0,
+        "rent_value": 50
+    },
+    {
+        "name": "Rumens Street",
+        "value": 50,
+        "rent_value": 10
+    },
+    {
+        "name": "Craigs Street",
+        "value": 50,
+        "rent_value": 10
+    },
+    {
+        "name": "Johns Street",
+        "value": 50,
+        "rent value": 5
+    },
+    {
+        "name": "Louises Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Free Parking",
+        "value": 0,
+        "rent_value": 0
+    },
+    {
+        "name": "Harrisons Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Maris Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Janeys Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Eugenes Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Maria Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Morag Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Castle Terrace",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Constitution Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Princes Street",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "The Royal Mile",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Leith",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Granton",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Waverly Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Haymarket Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "The Meadows",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Arthurs Seat",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Calton Hill",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Corstorphine Hill",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Vue Station",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "JavaScript Hill",
+        "value": 50,
+        "rent_value": 5
+    },
+    {
+        "name": "Vue Station",
+        "value": 50,
+        "rent_value": 5
+    }
+
+])
