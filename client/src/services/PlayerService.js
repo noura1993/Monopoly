7f0811413ff332abcadaf8f0802ca1d0ebf88acc
Ahmed Hamed <tunashark@gmail.com>
2020-07-03 21:25:25 +0100

    added getPlayers and updatePlayer to PlayerService

--- client/src/services/PlayerService.js
+++ client/src/services/PlayerService.js
@@ -1,8 +1,34 @@
-const baseURL = 'http://localhost:3000/api/properties/'
+const basePropertiesURL = 'http://localhost:3000/api/properties/';
+const basePlayersURL = 'http://localhost:3000/api/players/';
 
 export default {
     getProperties() {
-        return fetch(baseURL)
+        return fetch(basePropertiesURL)
         .then(res => res.json());
+    },
+
+    getPlayers() {
+        return fetch(basePlayersURL)
+        .then(res => res.json())
+    },
+
+    addPlayer(player) {
+        return fetch(basePlayersURL, {
+            method: 'POST',
+            body: JSON.stringify(player),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    },
+
+    updatePlayer(player) {
+        return fetch(basePlayersURL + player._id, {
+            method: 'PUT',
+            body: JSON.stringify(player),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     }
 }
