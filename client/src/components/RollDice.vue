636d85a9e9652cb08b398bc2a3ef2516e1f7a834
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-05 05:33:13 +0100

    Fix translateX and replace with translate as we are specifying both X and Y

--- client/src/components/RollDice.vue
+++ client/src/components/RollDice.vue
@@ -16,7 +16,7 @@ export default {
 .roll-dice-container {
     position: absolute;
     left: 50%;
-    transform:translateX(-50%, -50%);
+    transform:translate(-50%, -50%);
     top: 50%;
     background-color: rgb(255, 255, 255);
     border-radius: 30px;
