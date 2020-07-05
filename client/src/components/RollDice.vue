da754643610b5afc773ccd4e017a2398f660ee4c
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-05 04:26:13 +0100

    Add some css for RollDice

--- client/src/components/RollDice.vue
+++ client/src/components/RollDice.vue
@@ -13,5 +13,33 @@ export default {
 </script>
 
 <style>
+.roll-dice-container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;    
+    width: 23%;
+    height: 3%;
+    background-color: rgb(114, 184, 155);
+    border-radius: 30px;
+    padding: 1%;
+    margin: auto;
+}
+
+.btn {
+    font-size: 20px;
+    border-radius: 30px;
+    margin-left: 10px;
+}
+
+.btn:hover {
+    background-color: rgb(109, 192, 157);
+    cursor: pointer;
+}
+
+.btn:focus {
+    outline: none;
+}
 
 </style>
