f4fc87388088ecfd4cb6a760dbf29c282d3c7b85
Ronan Hughes <rhughes490@gmail.com>
2020-07-07 15:16:18 +0100

    Some CSS

--- client/src/components/PlayerForm.vue
+++ client/src/components/PlayerForm.vue
@@ -1,7 +1,7 @@
 <template lang="html">
-<div>
+<div class="container">
 <form v-on:submit.prevent="startGame">
-  <div class="form-wrapper">
+   <div class="form-wrapper">
   <div id="header">
     <img src="../assets/uncleJack.png" width="600" height="250" text-align: center/>
   </div>
@@ -38,7 +38,7 @@
         <option value="yellow">Yellow</option>
       </select>
       </div>
-    </div>
+      </div>
     <div class="bottom-players" v-if="this.numOfPlayers === '3' || this.numOfPlayers === '4'">
          <div class="player3">
         <label for="name">Player 3:</label>
@@ -63,14 +63,17 @@
       </select>
       </div>
     </div>
-  </div>
-    <input class="start-game" type="submit" value="Start Game"/>
     </div>
-  </form>
-  <button class="start-game" value="Continue Game" v-on:click="continueGame">Continue Game </button>
     <br>
-    <a href="https://en.wikibooks.org/wiki/Monopoly/Official_Rules#:~:text=Each%20player%20is%20given%20%241%2C500,personal%20funds%20from%20the%20bank." class="myButton2">Rules</a></p>
-    </div>
+    <input class="myButton" type="submit" value="Start Game"/>
+  </div>
+</form>
+<br>
+<button class="myButton" value="Continue Game" v-on:click="continueGame">Continue Game </button>
+<br>
+<br>
+<a href="https://en.wikibooks.org/wiki/Monopoly/Official_Rules#:~:text=Each%20player%20is%20given%20%241%2C500,personal%20funds%20from%20the%20bank." class="myButton2">Rules</a></p>
+</div>
 </template>
 
 <script>
@@ -142,13 +145,66 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.container {
+  text-align: center;
+}
+
+.players-container {
+  text-align: center;
+
+}
+
 .top-players {
-  display: flex;
+  text-align: center;
 }
 
 .bottom-players {
-  display: flex;
+  text-align: center;
 }
+
+.player1 {
+  display: inline-block;
+  text-align: center;
+}
+.player2 {
+  display: inline-block;
+  text-align: center;
+}
+
+.player3 {
+  display: inline-block;
+  text-align: center;
+}
+
+.player4 {
+  display: inline-block;
+  text-align: center;
+}
+
+.myButton {
+	box-shadow: 3px 4px 0px 0px #8a2a21;
+	background:linear-gradient(to bottom, #c62d1f 5%, #f24437 100%);
+	background-color:#c62d1f;
+	border-radius:18px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:7px 25px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #810e05;
+}
+.myButton:hover {
+	background:linear-gradient(to bottom, #f24437 5%, #c62d1f 100%);
+	background-color:#f24437;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
 
 /* .h1 {
   font-family: sans-serif;
@@ -187,3 +243,10 @@ export default {
   top: 1px;
 } */
 </style>
+
+
+
+  
+    
+    
+  
