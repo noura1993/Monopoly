f4cd317245f59b71c466573ffdac57c7b4856132
Ahmed Hamed <tunashark@gmail.com>
2020-07-08 11:43:44 +0100

    fixed playerSelling also being shown in drop down

--- client/src/components/PlayerInfo.vue
+++ client/src/components/PlayerInfo.vue
@@ -6,16 +6,17 @@
       <div>
         <p>Owned Properties:</p>
       <ul>
-        <li :player="player" v-for="property in player.properties" :key="property._id">{{property.name}} <a v-on:click="handleSell(property, player)" href="#">Sell</a></li>
+        <li :player="player" v-for="property in player.properties" :key="property._id">{{property.name}} <a v-if="!dropdownCondition" v-on:click="handleSell(property, player)" href="#">Sell</a></li>
         <form v-if="dropdownCondition" v-on:submit.prevent="handleSubmit">
         <select v-model="playerToSellTo">
           <option selected="true" disabled="disabled">Selling {{propertyBeingSold.name}}</option> 
-          <option v-for="(otherPlayer, index) in players" :value="otherPlayer" :key="index">{{otherPlayer.name}}</option>
+          <option v-if="otherPlayer !== player" v-for="(otherPlayer, index) in players" :value="otherPlayer" :key="index">{{otherPlayer.name}}</option>
         </select>
         <input type="number" v-model.number="sellValue" placeholder="Sell value...">
         <button type="submit">Sell {{propertyBeingSold.name}}</button>
-        <p></p>
+        <div>
         <a v-on:click="handleCancel" href="#">Cancel</a>
+        </div>
         </form>
       </ul>
     </div>
